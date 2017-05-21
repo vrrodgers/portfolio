22e0787f8ba0653abe5a8887d8963854146c6b67
vrrodgers <trulymissvicky1982@gmail.com>
2017-05-21 12:56:45 -0500

    created controller for albums,category, pages and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :album
+ resources :category
+ resources :contact, only: [:new, :create]
+ root to: "pages#home"
+ get 'about-me' to: 'pages#about_me' 
 end
